8aa563542039f15633cda943df857ad5ac5ff398
Kevin Chan <kvii.chan@gmail.com>
2015-05-12 19:26:54 -0400

    Fixed unit tests

--- src/common/hateoasClient/Controls/hateoasControlsController.js
+++ src/common/hateoasClient/Controls/hateoasControlsController.js
@@ -1,87 +1,93 @@
-angular.module('hateoas.controls.controller', 
-  ['hateoas.modal.controller',
-   'hateoas.controls.service',
-   'hateoas.utils',
-   'ngform-builder'])
-  .controller('HateoasControlsController', 
-    ['$scope', '$modal', '$location', 
-     'API', 'HateoasUtils', 'StatusService',
+(function() {
+  'use strict';
+  angular.module('hateoas.controls.controller', [
+    'hateoas.modal.controller',
+    'hateoas.controls.service',
+    'hateoas.utils',
+    'ngform-builder'
+  ])
+  .controller('HateoasControlsController', HateoasControlsController);
+
+  HateoasControlsController.$inject = [
+    '$scope', '$modal', '$location', 'API', 'HateoasUtils', 'StatusService'
+  ];
  
+  /**
+   * Controller for the directive
+   */
+  function HateoasControlsController($scope, $modal, $location, API, HateoasUtils, StatusService) {
+    // By default, the HateoasService is used. However, the service can be
+    // overridden by declaring the service in the directive.
+    var Service = HateoasUtils.getService('ControlsService');
+
     /**
-     * Controller for the directive
+     * Private: archive
+     * Archives an item on the API.
+     * @param   item - item to archive
+     * @returns $promise
      */
-    function HateoasControlsController($scope, $modal, $location, API, HateoasUtils, StatusService) {
-      // By default, the HateoasService is used. However, the service can be
-      // overridden by declaring the service in the directive.
-      var Service = HateoasUtils.getService('ControlsService');
+    function archive(item) {
+      Service.archive(item);
+    }
 
-      /**
-       * Private: archive
-       * Archives an item on the API.
-       * @param   item - item to archive
-       * @returns $promise
-       */
-      function archive(item) {
-        Service.archive(item);
-      }
+    /**
+     * Private: edit
+     * Launches a form to edit or create an item. Uses form-builder to draw the
+     * form.
+     * @param   item - item to create/update
+     * @returns null
+     */
+    function open(method) {
+      var modalScope = $scope.$new(true);
+      modalScope.item = (method === 'post' ? 
+          {} : angular.copy($scope.item));
+      modalScope.template = $scope.template;
 
-      /**
-       * Private: edit
-       * Launches a form to edit or create an item. Uses form-builder to draw the
-       * form.
-       * @param   item - item to create/update
-       * @returns null
-       */
-      function open(method) {
-        var modalScope = $scope.$new(true);
-        modalScope.item = (method === 'post' ? 
-            {} : angular.copy($scope.item));
-        modalScope.template = $scope.template;
+      var instance = $modal.open({
+        templateUrl: 'hateoasClient/Modal/hateoasModal.tpl.html',
+        controller: 'HateoasModalController',
+        size: 'lg',
+        scope: modalScope
+      });
 
-        var instance = $modal.open({
-          templateUrl: 'hateoasClient/Modal/hateoasModal.tpl.html',
-          controller: 'HateoasModalController',
-          size: 'lg',
-          scope: modalScope
-        });
-
-        instance.result.then(function(item) {
-          var newItem = _.merge(modalScope.item, item);
-          var api = newItem.href || $scope.href;
-          //TODO: Handle errors!
-          Service.commit(api, newItem).then(function(data) {
-            StatusService.update({
-              type: 'success',
-              msg: 'Item successfully updated!'
-            });
-          }, function (err) {
-            StatusService.update({
-              type: 'error',
-              msg: err.data.raw.err
-            });            
-          }).then(function() {
-            $scope.$emit('hateoas.client.refresh');
-            modalScope.$destroy();
+      instance.result.then(function(item) {
+        var newItem = _.merge(modalScope.item, item);
+        var api = newItem.href || $scope.href;
+        //TODO: Handle errors!
+        Service.commit(api, newItem).then(function(data) {
+          StatusService.update({
+            type: 'success',
+            msg: 'Item successfully updated!'
           });
+        }, function (err) {
+          StatusService.update({
+            type: 'error',
+            msg: err.data.raw.err
+          });            
+        }).then(function() {
+          $scope.$emit('hateoas.client.refresh');
+          modalScope.$destroy();
         });
+      });
+    }
+
+    /**
+     * Private: defaultLauncher
+     * The default button handler
+     */
+    $scope.launch = function defaultLauncher(button) {
+      if (button.method === 'get') {
+        if ($scope.item.rel) {
+          var index = $scope.item.href.indexOf(API.prefix) + API.prefix.length;
+          $location.path($scope.item.href.substring(index));
+        }
       }
+      else if (button.method === 'delete') {
+        archive($scope.item);
+      } else {
+        open(button.method);
+      }
+    };
 
-      /**
-       * Private: defaultLauncher
-       * The default button handler
-       */
-      $scope.launch = function defaultLauncher(button) {
-        if (button.method === 'get') {
-          if ($scope.item.rel) {
-            var index = $scope.item.href.indexOf(API.prefix) + API.prefix.length;
-            $location.path($scope.item.href.substring(index));
-          }
-        }
-        else if (button.method === 'delete') {
-          archive($scope.item);
-        } else {
-          open(button.method);
-        }
-      };
-
-    }]);
+  }
+})();
--- src/common/hateoasClient/Controls/hateoasControlsService.js
+++ src/common/hateoasClient/Controls/hateoasControlsService.js
@@ -1,28 +1,35 @@
-angular.module('hateoas.controls.service', ['ngResource'])
-  .service('HateoasControlsService', [ '$q', '$resource',
-  function($q, $resource) {
-      /**
-       * Public: commit
-       * Commits a HATEOAS item to the API.
-       * @param   item - item to create/update on the API.
-       * @returns $promise
-       */
-      this.commit = function commit(href, item) {
-        var resource = $resource(href, null, { 
-          'update': { method: 'PUT' },
-          'save': { method: 'POST' }
-        });
-        var method = (_.has(item, 'id') ? 'update' : 'save');
-        return resource[method](item).$promise;
-      };
+(function() {
+  'use strict';
+  angular
+    .module('hateoas.controls.service', ['ngResource'])
+    .service('HateoasControlsService', HateoasControlsService);
 
-      /**
-       * Public: archive
-       * Archive a HATEOAS item from the API
-       * @param   item - item to archive
-       * @returns $promise
-       */
-      this.archive = function archive(item) {
-        console.log("Not implemented!");
-      };
-  }]);
+  HateoasControlsService.$inject = ['$q', '$resource'];
+  
+  function HateoasControlsService($q, $resource) {
+    /**
+     * Public: commit
+     * Commits a HATEOAS item to the API.
+     * @param   item - item to create/update on the API.
+     * @returns $promise
+     */
+    this.commit = function commit(href, item) {
+      var resource = $resource(href, null, { 
+        'update': { method: 'PUT' },
+        'save': { method: 'POST' }
+      });
+      var method = (_.has(item, 'id') ? 'update' : 'save');
+      return resource[method](item).$promise;
+    };
+
+    /**
+     * Public: archive
+     * Archive a HATEOAS item from the API
+     * @param   item - item to archive
+     * @returns $promise
+     */
+    this.archive = function archive(item) {
+      console.log("Not implemented!");
+    };
+  }
+})();
