1cfeb2485a11bc1f3e398ef1afbf000a9b929488
Kevin Chan <kvii.chan@gmail.com>
2015-07-14 13:22:20 -0400

    Added User docstrings

--- api/models/User.js
+++ api/models/User.js
@@ -17,49 +17,108 @@
 
     schema: true,
     attributes: {
+      /**
+       * firstname
+       * @description A user's first name.
+       * @type {String}
+       */
       firstname: {
         type: 'string'
       },
+
+      /**
+       * lastname
+       * @description A user's last name.
+       * @type {String}
+       */
       lastname: {
         type: 'string'
       },
+
+      /**
+       * prefix
+       * @description Enumeration of allowable prefixes for a user.
+       * @type {Enum}
+       */
       prefix: {
         type: 'string',
         enum: ['Mr.', 'Mrs.', 'Ms.', 'Dr.']
       },
+
+      /**
+       * gender
+       * @description Enumeration of allowable genders of a user.
+       * @type {Enum}
+       */
       gender: {
         type: 'string',
         enum: ['Male', 'Female']
       },
+
+      /**
+       * dob
+       * @description A user's date of birth.
+       * @type {Date}
+       */
       dob: {
         type: 'date'
       },
-      // group of roles this user has
+
+      /**
+       * group
+       * @description A user's registered group which in turn dictates what actions
+       *              this user is permitted to perform.
+       * @type {Association}
+       */
       group: {
         model: 'group'
       },
-      // coordinator/interviewer enrollments at CCs I am overseeing
+
+      /**
+       * enrollments
+       * @description Linked associations of UserEnrollments denoting which collection
+       *              centres/studies this user is overseeing as a coordinator/interviewer.
+       * @type {Association}
+       */
       enrollments: {
         collection: 'userenrollment',
         via: 'user'
       },
+
+      /**
+       * expiredAt
+       * @description Instead of strictly deleting objects from our system, we set a date such
+       *              that if it is not null, we do not include this entity in our response.
+       * @type {Date} Date of expiry
+       */
       expiredAt: {
         type: 'datetime',
         defaultsTo: null,
         datetime: true
       },
+
       toJSON: HateoasService.makeToHATEOAS.call(this, module)
     },
 
+    /**
+     * afterCreate
+     * @description Lifecycle function that fires after a User is created.  We
+     *              do two things whenever we create a user:  set the owner attribute
+     *              to themselves so that we can allow users to read/update themselves
+     *              only.  Afterwards, we grant them roles based on the group we
+     *              assigned them during their creation.
+     *
+     * @type {Array} Functions that are called in sequence following user creation
+     */
     afterCreate: [
-      function setOwner (user, next) {
+      function setOwner (user, cb) {
         sails.log('User.afterCreate.setOwner', user);
         User
           .update({ id: user.id }, { owner: user.id })
           .then(function (user) {
-            next();
+            cb();
           })
-          .catch(next);
+          .catch(cb);
       },
       function grantRoles(user, cb) {
         if (_.has(user, 'group')) {
@@ -73,6 +132,31 @@
         }
       }
     ],
+
+    /**
+     * afterUpdate
+     * @description Lifecycle callback meant to handle deletions in our system; if at
+     *              any point we set this collection centre's expiredAt attribute, this
+     *              function will check and invalidate any users/subjects still enrolled
+     *              in this collection centre.
+     *
+     * @param  {Object}   updated updated collection centre object
+     * @param  {Function} cb      callback function on completion
+     */
+    afterUpdate: function(updated, cb) {
+      if (!_.isNull(updated.expiredAt)) {
+        UserEnrollment.update({ collectionCentre: updated.id }, { expiredAt: new Date() })
+        .then(function (userEnrollments) {
+          return SubjectEnrollment.update({ collectionCentre: updated.id }, { expiredAt: new Date() });
+        })
+        .then(function (subjectEnrollments) {
+          cb();
+        })
+        .catch(cb);
+      } else {
+        cb();
+      }
+    }
 
     findByStudyName: function(studyName, currUser, options, cb) {
       EnrollmentService
