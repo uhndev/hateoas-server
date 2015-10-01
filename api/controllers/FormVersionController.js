812ce74ab80671f9c9346f989d8cbae18fd678e2
Ivan Shibanov <ivan@technainstitute.com>
2015-10-01 15:19:57 -0400

    Moved logic into controller

--- api/controllers/FormVersionController.js
+++ api/controllers/FormVersionController.js
@@ -8,29 +8,36 @@
 module.exports = {
   create: function(req, res, next) {
     var formID = req.param('id');
-    var options = _.pick(req.body, 'name', 'metaData', 'questions');
 
-    // create new form version with updated revision number
-    FormVersion.find({ form: formID })
-      .sort('revision DESC')
-      .then(function (latestFormVersions) {
-        var newFormVersion = {
-          revision: _.first(latestFormVersions).revision + 1,
-          form: formID
-        };
-        _.merge(newFormVersion, options);
-        return FormVersion.create(newFormVersion);
-      })
-      .then(function (newFormVersion) {
-        res.status(201).jsonx(centre);
-      })
-      .catch(function (err) {
-        res.badRequest({
-          title: 'Error',
-          code: err.status,
-          message: err.details
-        });
+    Form.findOne(formID).then(function (form){
+      // if lastPublished set on Form, then there are AnswerSets referring to this version
+      if(form.lastPublished !== null) {
+        var options = _.pick(req.body, 'name', 'metaData', 'questions');
+        
+        // create new form version with updated revision number
+        FormVersion.find({ form: formID })
+          .sort('revision DESC')
+          .then(function (latestFormVersions) {
+            sails.log(latestFormVersions);
+            var newFormVersion = {
+              revision: _.first(latestFormVersions).revision + 1,
+              form: formID
+            };
+            _.merge(newFormVersion, options);
+            return FormVersion.create(newFormVersion);
+          })
+          .then(function (newFormVersion) {
+            res.status(201).jsonx(newFormVersion);
+          });
+      }
+    })
+    .catch(function (err) {
+      res.badRequest({
+        title: 'Error',
+        code: err.status,
+        message: err.details
       });
+    });
   },
 };
 
