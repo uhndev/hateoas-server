434ead5801cc87eaadb35966d17ac37110ffbb6d
Ivan Shibanov <ivan@technainstitute.com>
2015-10-01 16:12:45 -0400

    Always return a response

--- api/controllers/FormVersionController.js
+++ api/controllers/FormVersionController.js
@@ -10,26 +10,27 @@ module.exports = {
     var formID = req.param('id');
 
     Form.findOne(formID).then(function (form){
-      // if lastPublished set on Form, then there are AnswerSets referring to this version
-      if(form.lastPublished !== null) {
-        var options = _.pick(req.body, 'name', 'metaData', 'questions');
-        
-        // create new form version with updated revision number
-        FormVersion.find({ form: formID })
-          .sort('revision DESC')
-          .then(function (latestFormVersions) {
-            sails.log(latestFormVersions);
+      var options = _.pick(req.body, 'name', 'metaData', 'questions');
+      
+      FormVersion.find({ form: formID })
+        .sort('revision DESC')
+        .then(function (latestFormVersions) {
+          // if lastPublished set on Form, then there are AnswerSets referring to this version
+          if (form.lastPublished !== null) {
+            // create new form version with updated revision number
             var newFormVersion = {
               revision: _.first(latestFormVersions).revision + 1,
               form: formID
             };
             _.merge(newFormVersion, options);
             return FormVersion.create(newFormVersion);
-          })
-          .then(function (newFormVersion) {
-            res.status(201).jsonx(newFormVersion);
-          });
-      }
+          } else {
+            return _.first(latestFormVersions);
+          }
+        })
+        .then(function (formVersion) {
+          res.created(formVersion);
+        });
     })
     .catch(function (err) {
       res.badRequest({
