7346af40a5ecbf1f9b631e48d374aedf79a93b1d
Walter Osmond <walter.osmond@uhn.ca>
2015-11-09 11:16:51 -0500

    added workstatus models, fixtures for menus in groups

---
+++ api/controllers/WorkStatusController.js
@@ -0,0 +1,16 @@
+/**
+ * WorkStatusController
+ *
+ * @module  controllers/WorkStatus
+ * @description Server-side logic for managing WorkStatuss
+ * @help        See http://links.sailsjs.org/docs/controllers
+ */
+
+(function() {
+
+  var actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil');
+
+  module.exports = {
+
+  };
+})();
---
+++ api/models/altum/WorkStatus.js
@@ -0,0 +1,37 @@
+/**
+ * WorkStatus
+ *
+ * @class workStatus
+ * @description Model representation of a workStatus
+ * @extends https://github.com/tjwebb/sails-permissions/edit/master/api/models/workStatus.js
+ * @extends https://github.com/tjwebb/sails-auth/edit/master/api/models/workStatus.js
+ */
+
+  (function () {
+
+    var _super = require('./BaseModel.js');
+    var _ = require('lodash');
+    var HateoasService = require('../../services/HateoasService.js');
+
+    _.merge(exports, _super);
+    _.merge(exports, {
+
+    schema: true,
+    attributes: {
+
+      /**
+       * workStatusName
+       * @description A workStatus's name
+       * @type {Date}
+       */
+
+      name: {
+        type: 'string'
+      },
+      toJSON: HateoasService.makeToHATEOAS.call(this, module)
+
+
+    }
+    });
+  })();
+
