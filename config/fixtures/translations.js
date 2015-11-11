136a11025dacf229d97fd4f04ab6545dbe8f7e75
Kevin Chan <kvii.chan@gmail.com>
2015-11-11 09:47:33 -0500

    chore(localization): Added altum translation to defaults, added script to dump from mongo to config/locales

--- config/fixtures/translations.js
+++ config/fixtures/translations.js
@@ -16,12 +16,17 @@
         language: 'fr',
         translationKey: 'COMMON.LANGUAGES.FRENCH',
         translation: require('../locales/fr.json')
+      },
+      {
+        language: 'altum',
+        translationKey: 'COMMON.LANGUAGES.ALTUM',
+        translation: require('../locales/altum.json')
       }
     ];
 
     return Promise.all(
       _.map(translations, function (translation) {
-        return Translation.findOrCreate(translation, translation);
+        return Translation.findOrCreate({ language: translation.language }, translation);
       })
     );
   };
