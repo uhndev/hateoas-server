8998f548ec0e78bfec044b7735cddd019d2e5fe0
Ivan Shibanov <ivan@technainstitute.com>
2015-11-19 10:53:33 -0500

    Modified AnswerSet model, added schedule route, controller methods

--- api/controllers/AnswerSetController.js
+++ api/controllers/AnswerSetController.js
@@ -9,7 +9,72 @@
 (function() {
 
   module.exports = {
-
+    /**
+     * create
+     * @description Creates a new answerset given a schedule, subject enrollment,
+     *              session, form and answers array.
+     */
+    create: function(req, res, next) {
+      var sessionID = req.param('sessionID'),
+          scheduleID = req.param('scheduleID'),
+          subjectEnrollmentID = req.param('subjectEnrollmentID'),
+          formID = req.param('formID'),
+          answers = req.param('answers');
+          
+      console.log(answers);
+      
+      studysession.findOne({ id: sessionID })
+        .then(function (studysess) {
+          if (_.isUndefined(studysess)) {
+            err = new Error('Session '+sessionID+' does not exist.');
+            err.status = 400;
+            throw err;
+          } else {
+            this.studySessionView = studysess;
+            
+            return SubjectEnrollment.findOne(subjectEnrollmentID);
+          }
+        })
+        .then(function (sEnroll) {
+          if (_.isUndefined(sEnroll)) {
+            err = new Error('SubjectEnrollment '+subjectEnrollmentID+' does not exist.');
+            err.status = 400;
+            throw err;
+          } else {
+            return UserEnrollment.findOne({
+              user: req.user.id,
+              collectionCentre: sEnroll.collectionCentre
+            });
+          }
+        })
+        .then(function (uEnroll) {
+          // user enrollment can be null
+          var userEnrollmentID = null;
+          if (!_.isUndefined(uEnroll)) {
+            userEnrollmentID = uEnroll.id;
+          }
+          
+          return AnswerSet.create({
+            answers : answers,
+            study : this.studySessionView.study,
+            surveyVersion : this.studySessionView.surveyVersion,
+            formVersion : formID,
+            subjectSchedule : scheduleID,
+            subjectEnrollment : subjectEnrollmentID,
+            userEnrollment : userEnrollmentID,
+          });
+        })
+        .then(function (answerSet) {
+          res.status(201).jsonx(answerSet);
+        })
+        .catch(function (err) {
+          res.badRequest({
+            title: 'Error',
+            code: err.status,
+            message: err.details
+          });
+        });
+    },
   };
 })();
 
--- api/controllers/SubjectScheduleController.js
+++ api/controllers/SubjectScheduleController.js
@@ -5,7 +5,49 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
-	
-};
+(function() {
+  var moment = require('moment'); // I need a minute
+  var _ = require('lodash');
+  var actionUtil = require('../../node_modules/sails/lib/hooks/blueprints/actionUtil');
+  var Promise = require('q');
 
+  module.exports = {
+
+    findScheduledForm: function(req, res, next) {
+      var formID = req.param('formID');
+      
+      SubjectSchedule.findOne(req.param('id')).populate('answerSets').exec(function (err, schedule) {
+        sails.log.debug(schedule);
+        var response = _.pick(schedule, 'id', 'status', 'session', 'subjectEnrollment');
+        sails.log.debug(response);
+        // use schedule.subjectEnrollment
+        
+        Session.findOne({id: schedule.session})
+          .populate('formVersions')
+          .then(function (session) {
+            return _.find(session.formVersions, function (formVersion) {
+              return formVersion.id == formID;
+            });
+          })
+          .then(function (formVersion) {
+            if(formVersion === undefined) {
+              res.notFound();
+            } else {
+              var answerSet = _.find(schedule.answerSets, function (as) {
+                return as.formVersion == formID;
+              });
+              if (answerSet !== undefined) {
+                formVersion.answerSetID = answerSet.id;
+              }
+              formVersion.subjectEnrollmentID = schedule.subjectEnrollment;
+              formVersion.scheduleID = schedule.id;
+              formVersion.sessionID = schedule.session;
+              res.ok(formVersion);
+            }
+          });
+      });
+    }
+    
+  };
+
+})();
