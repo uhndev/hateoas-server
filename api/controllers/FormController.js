49f58515d308055e6c304333edff91d72399b45f
Kevin Chan <kvii.chan@gmail.com>
2015-11-30 09:02:25 -0500

    fix(forms): added pre-check for answersets before form archive

--- api/controllers/FormController.js
+++ api/controllers/FormController.js
@@ -12,6 +12,22 @@
   _.merge(exports, StudyBase); // inherits StudyBaseController.findByStudyName
   _.merge(exports, {
 
+    destroy: function (req, res) {
+      var formID = req.param('id'); // form primary key to remove
+
+      FormVersion.hasAnswerSets(formID).then(function (hasAnswerSets) {
+        if (hasAnswerSets) {
+          return res.badRequest({
+            title: 'Form Error',
+            code: 400,
+            message: 'Unable to archive form, there are answers associated to the requested form.'
+          });
+        } else {
+          return require('../blueprints/destroy')(req, res);
+        }
+      });
+    }
+
   });
 
 })();
