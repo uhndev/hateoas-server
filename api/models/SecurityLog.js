41f0b5468a1db5f492d19a8602c59a5dd89daa5d
Walter Osmond <walter.osmond@uhn.ca>
2015-12-02 16:33:36 -0500

    removed baselog form seucritylog

--- api/models/SecurityLog.js
+++ api/models/SecurityLog.js
@@ -1,12 +1,10 @@
 // api/models/SecurityLog.js
 
 (function() {
-  var _super = require('./BaseModel.js');
 
   var _ = require('lodash');
   var _log = require('sails-permissions/api/models/SecurityLog');
 
-  _.merge(exports, _super);
   _.merge(exports, _log);
   _.merge(exports, {
 
