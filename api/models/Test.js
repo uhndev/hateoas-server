541f3d3e42c2711d97f4e2390abb0af187a3bfe6
Walter Osmond <walter.osmond@uhn.ca>
2015-12-05 01:49:18 -0500

    changed HATEOAS templates to use _attribute object instead of definition

---
+++ api/models/Test.js
@@ -0,0 +1,31 @@
+/**
+ * Role
+ *
+ * @class Role
+ * @description Roles endow Users with Permissions. Exposes Postgres-like API for
+ *              resolving granted Permissions for a User.
+ * @extends https://github.com/tjwebb/sails-permissions/blob/master/api/models/Role.js
+ */
+
+
+(function() {
+  var _super = require('./BaseModel.js');
+
+  var _ = require('lodash');
+  var _role = require('sails-permissions/api/models/Role');
+
+  _.merge(exports, _super);
+  _.merge(exports, _role);
+  _.merge(exports, {
+
+    // Extend with custom logic here by adding additional fields, methods, etc.
+
+    attributes: {
+      groups: {
+        collection: 'group',
+        via: 'roles'
+      }
+    }
+  });
+})();
+
