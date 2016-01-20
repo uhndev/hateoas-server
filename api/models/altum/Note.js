c7ebba29e86e6df193df8713f4ab3105bb47242a
Calvin Su <calvin.su@uhn.ca>
2016-01-20 11:48:40 -0500

    add the comment for model

--- api/models/altum/Note.js
+++ api/models/altum/Note.js
@@ -8,15 +8,38 @@
 module.exports = {
 
     attributes: {
+
+        /**
+         * name
+         * @description note text
+         * @type {String}
+         */
         text: {
             type: 'string'
         },
+
+        /**
+         * name
+         * @description A referral which the note belong to
+         * @type {Model}
+         */
         referral: {
             model: 'referral'
         },
+        /**
+         * name
+         * @description A client which the note belong to
+         * @type {Model}
+         */
         client: {
             model: 'client'
         },
+
+        /**
+         * name
+         * @description A note type
+         * @type {Model}
+         */
         noteType: {
             model: 'NoteType'
         }
--- api/models/altum/NoteType.js
+++ api/models/altum/NoteType.js
@@ -8,12 +8,29 @@
 module.exports = {
 
     attributes: {
+
+        /**
+         * name
+         * @description A note type name
+         * @type {string}
+         */
         name: {
             type: 'string'
         },
+
+        /**
+         * name
+         * @description A note type icon will show in html
+         * @type {Model}
+         */
         iconClass: {
             type: 'string'
         },
+        /**
+         * name
+         * @description note collection
+         * @type {Model}
+         */
         notes: {
             collection: 'note',
             via: 'noteType'
