304303fdff6b5dbacbcdf1de1bc29d0b9b671199
Kevin Chan <kvii.chan@gmail.com>
2016-04-01 14:15:07 -0400

    Added group/role fixtures for altumadmin group

--- config/fixtures/role.js
+++ config/fixtures/role.js
@@ -8,6 +8,60 @@
   exports.create = function () {
 
     return Promise.all([
+      Role.findOneByName('altumadmin').then(function (role) {
+        if (!role) {
+          var permissions = [
+            { model: 'systemform',        action: 'read' },
+            { model: 'translation',       action: 'read' },
+            {
+              model: 'group',
+              action: 'read',
+              criteria: [
+                {
+                  where: { level: 2 }
+                }
+              ]
+            },
+            {
+              model: 'user',
+              action: 'read',
+              criteria: [
+                {
+                  where: { group: { '!': 'subject' } },
+                  blacklist: ['group']
+                }
+              ]
+            },
+            {
+              model: 'user',
+              action: 'update',
+              relation: 'owner'
+            }
+          ];
+
+          var altumPermissions = _.reduce([
+            'address', 'altumservice', 'approval', 'approver', 'city', 'client',
+            'company', 'emergencycontact', 'employee', 'invoice', 'language',
+            'note', 'notetype', 'payor', 'physician', 'prognosis', 'program',
+            'programservice', 'referral', 'service', 'servicecategory', 'site',
+            'staff', 'stafftype', 'status', 'timeframe', 'workstatus', 'person',
+            'altumprogramservices', 'clientcontact', 'referraldetail', 'servicedetail'
+          ], function (result, model) {
+            return result.concat(_.map(['create', 'read', 'update', 'delete'], function (action) {
+              return {
+                model: model,
+                action: action
+              }
+            }));
+          }, []);
+
+          return PermissionService.createRole({
+            name: 'altumadmin',
+            permissions: permissions.concat(altumPermissions)
+          });
+        }
+        return role;
+      }),
       Role.findOneByName('coordinator').then(function (role) {
         if (!role) {
           return PermissionService.createRole({
