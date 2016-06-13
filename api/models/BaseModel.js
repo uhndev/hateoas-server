e71bf6d8c461c23d2a41a1deb32df99427809ff7
eduardo <eduardomartins.java@gmail.com>
2016-06-13 15:26:11 -0300

    DAD-1440 Fhir interface

--- api/models/BaseModel.js
+++ api/models/BaseModel.js
@@ -4,7 +4,6 @@
  * @description the basemodel to be inherited by all other models
  */
 var _ = require('lodash');
-var Promise = require('bluebird');
 
 module.exports = {
 
@@ -57,28 +56,6 @@ module.exports = {
   beforeValidate: function (values, cb) {
     // for each field listed in default, check values for those fields and add to display
     var display = _.values(_.pick(values, this.displayFields)).join(' ');
-
-
-
-      //var fhir=FhirService.search({type: 'Patient', query: {'birthdate': '1974'}})
-      //  .then(function (res) {
-      //    var bundle = res.data;
-      //    var count = (bundle.entry && bundle.entry.length) || 0;
-      //    console.log("# Patients born in 1974: ", count);
-      //    return bundle.entry.length;
-      //  })
-      //  .catch(function (res) {
-      //    //Error responses
-      //    if (res.status) {
-      //      console.log('Error', res.status);
-      //    }
-      //
-      //    //Errors
-      //    if (res.message) {
-      //      console.log('Error', res.message);
-      //    }
-      //  });
-
 
     // if display fields are found in values set the displayName, otherwise set default
     values.displayName = display ? display : 'No Display Name';
