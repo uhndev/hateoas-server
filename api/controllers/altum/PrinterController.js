253cbd225e3771c34de3b27181d1b53a25f7d43f
Calvin Su <calvin.su@uhn.ca>
2016-06-17 09:36:24 -0400

    label printing added printer model

---
+++ api/controllers/altum/PrinterController.js
@@ -0,0 +1,14 @@
+/**
+ * PhysicianController
+ *
+ * @module  controllers/printer
+ * @description Server-side logic for managing printer
+ * @help        See http://links.sailsjs.org/docs/controllers
+ */
+
+(function() {
+  module.exports = {
+    identity: 'Printer',
+
+  };
+})();
