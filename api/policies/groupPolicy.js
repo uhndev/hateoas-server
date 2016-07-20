1a64b049bce50aa5097dba6c97fb380e0bdff6eb
Kevin Chan <kvii.chan@gmail.com>
2016-07-20 12:03:27 -0400

    initial commit: added new policy for preventing deletion of key groups

---
+++ api/policies/groupPolicy.js
@@ -0,0 +1,17 @@
+/**
+ * GroupPolicy
+ *
+ * @module      Policies
+ * @description Policy for ensuring the default admin groups are not deleted
+ * @docs        http://sailsjs.org/#!documentation/policies
+ *
+ */
+module.exports = function(req, res, next) {
+  var groupID = req.param('id');
+  var action = PermissionService.getMethod(req.method);
+  if (action == 'delete' && groupID === 'admin' || groupID === 'altumadmin') {
+    return res.forbidden('You are not permitted to perform this action.');
+  } else {
+    return next();
+  }
+};
