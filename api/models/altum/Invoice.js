ba55598302053fc9f1649c0a0c74463098557fdb
Kevin Chan <kvii.chan@gmail.com>
2016-08-31 12:03:17 -0400

    initial commit: added lifecycle voiding for Invoices, included quick fix for ARM-812 as well

--- api/models/altum/Invoice.js
+++ api/models/altum/Invoice.js
@@ -116,12 +116,36 @@
      * @param  {Function} cb      callback function on completion
      */
     afterUpdate: function(updated, cb) {
-      if (!_.isNull(updated.expiredAt)) {
-        InvoiceService.update({ invoice: updated.id }, { expiredAt: new Date() }).exec(function (err, invoiceServices) {
-          cb(err);
-        });
-      } else {
-        cb();
+      switch (true) {
+        case !_.isNull(updated.expiredAt):
+          return InvoiceService.update({ invoice: updated.id }, { expiredAt: new Date() })
+            .then(function (invoiceServices) {
+              cb();
+            }).catch(cb);
+          break;
+        case updated.status === 'Voided':
+          return InvoiceService.find({invoice: updated.id})
+            .then(function (invoiceServices) {
+              return [
+                Service.find({id: _.map(invoiceServices, 'service')}),
+                Status.findOne({systemName: 'SUSPENDED'})
+              ];
+            })
+            .spread(function (services, suspendedStatus) {
+              return Promise.all(_.map(services, function (service) {
+                return BillingStatus.create({
+                  status: suspendedStatus.id,
+                  service: service.id,
+                  createdBy: 1,
+                  owner: 1
+                });
+              }));
+            })
+            .then(function (servicePromises) {
+              cb();
+            }).catch(cb);
+        default: 
+          return cb();
       }
     },
 
